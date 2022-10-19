46c63f7b4e1f9c95a4e37dcdb17507458ebcd813
JonataSzLm <jonaatslima5@gmail.com>
2022-10-19 20:32:12 -0300

    autenticação (ainda desatualizado) FUNCIONANDO

--- src/api/common/errorHandler.js
+++ src/api/common/errorHandler.js
@@ -3,7 +3,7 @@ const _ = require('lodash')
 module.exports = (req, res, next) => {
     const bundle = res.locals.bundle
 
-    if (bundle.errors) {
+    if(bundle.errors) {
         const errors = parseErrors(bundle.errors)
         res.status(500).json({errors})
     } else {
@@ -11,8 +11,8 @@ module.exports = (req, res, next) => {
     }
 }
 
-const parseErrors = (nodeRestfulErros) => {
+const parseErrors = (nodeRestfulErrors) => {
     const errors = []
-    _.forIn(nodeRestfulErros, error => errors.push(error.message))
-    return errors
+    _.forIn(nodeRestfulErrors, error => errors.push(error.message))
+    return errors 
 }
